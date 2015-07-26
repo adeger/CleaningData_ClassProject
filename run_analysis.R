ad25316d7c2867f4f2114347e16dac0424e00c4e
Alan Deger <al@deger-ford.com>
2015-07-26 03:58:41 -0600

    Update run_analysis.R with better documentation and prototype foo removed

--- run_analysis.R
+++ run_analysis.R
@@ -5,20 +5,30 @@
 # The script satisfies the requirements of the Coursera course "Getting and Cleaning Data" 
 # class project:
 #
-#  1) #  2) 
+#  1) Merges training and data sets (variables containing mean() and std()), subject (number) and
+#     activity using the activity name instead of the activity code
+#  2) Outputs a tidy data set grouped by subject (number) and activity of means for those observation
+#     groups
+#
+# The following file paths must be visible from the current R working directory:
+#   - ./activity_labels.txt
+#   - ./features.txt
+#   - ./features_info.txt
+#   - ./test/subject_test.txt
+#   - ./test/X_test.txt
+#   - ./test/y_test.txt
+#   - ./train/subject_train.txt
+#   - ./train/X_train.txt
+#   - ./train/y_train.txt
+# 
+# to run source this directory
 
-library(dplyr)
-
-check_for_files <- function () {
-    files_needed <- c("./activity_labels.txt", "./features.txt", "./features_info.txt",
-                      "./test/subject_test.txt", "./test/X_test.txt", "./test/y_test.txt",
-                      "./train/subject_train.txt", "./train/X_train.txt", "./train/y_train.txt")
-
-    
-}
+library(dplyr)   # required library
 
 merge_data <- function(data_list){
-
+    # return a data frame of merged data (combining "test" and "training" subjects) from the 
+    # list output by read_data, excluding accelerometer data variables not reporting 
+    # means or standard deviations 
 	
     all_x <- rbind(data_list$x_train, data_list$x_test)
     colnames(all_x) <- data_list$features[,2]
@@ -42,6 +52,8 @@ merge_data <- function(data_list){
 }
 
 read_data <- function(){
+    # return a list of data frames from each of the required files in the data set
+
     res <- list()
     res$x_train <- read.table("./train/X_train.txt", header=FALSE)
     res$y_train <- read.table("./train/y_train.txt", header=FALSE)
@@ -58,11 +70,14 @@ read_data <- function(){
 }
 
 summarize_data <- function(merged_data){
+    # return a data frame of summary values for each of the variables grouped
+    # by unique combinations of subject and activity
+
     summarise_each(group_by(merged_data, Subject, Activity), c("mean"))
 }
 
 do_all <- function() {
-    #check_for_files()
+    # run the whole shmear
 
     data_list <- read_data()
     merged_data <- merge_data(data_list)
